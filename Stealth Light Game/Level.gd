61d14edf851d830529e3d93833d24da3aa219666
Doots <65699806+Dootsu@users.noreply.github.com>
2020-05-26 12:13:57 -0400

    Added Editor
    Levels are generated from text files

--- Stealth Light Game/Level.gd
+++ Stealth Light Game/Level.gd
@@ -6,13 +6,28 @@ const shiftZ=Vector2(-24,44);
 
 var origin=Vector2(200,200);
 var blockTemplate=[];
+var blockInstance=[];
 func _ready():
 	blockTemplate.append(preload("res://BlockBase.tscn"));
-	addBlock(0,0,0);
-	addBlock(1,1,1);
+	genLvl("lvl0.txt");
 	
-func addBlock(pX,pY,pZ):
-	var a=blockTemplate[0].instance();
+func addBlock(type,pX,pY,pZ):
+	var a=blockTemplate[type].instance();
 	a.scale=Vector2(.2,.2);
 	a.position=origin+shiftX*pX+shiftY*pY+shiftZ*pZ;
 	self.add_child(a);
+
+func genLvl(filename):
+	var fid=File.new();
+	fid.open("res://lvl/"+filename,File.READ);
+	while not fid.eof_reached():
+		var currLine=fid.get_line();
+		if len(currLine) and currLine[0]!='#':
+			var a=currLine.split(" ",true);
+			if len(a)<7:
+				a.resize(7);
+				a[4]=a[1];a[5]=a[2];a[6]=a[3];
+			for x in range(int(a[1]),int(a[4])+1):
+				for y in range(int(a[2]),int(a[5])+1):
+					for z in range(int(a[3]),int(a[6])+1):
+						addBlock(int(a[0]),x,y,z);
