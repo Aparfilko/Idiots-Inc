87be8d5b26d2c585f52abd7125bc4ad5061ba3cf
Doots <65699806+Dootsu@users.noreply.github.com>
2020-05-26 13:20:13 -0400

    Z_Index sort + shift
    blocks store position, and update their z_index variable based on it. Also added arrow key movement to let me look around while i test

--- Stealth Light Game/Level.gd
+++ Stealth Light Game/Level.gd
@@ -1,8 +1,9 @@
 extends Node2D
 
-const shiftX=Vector2(53,22);
-const shiftY=Vector2(0,-29);
-const shiftZ=Vector2(-24,44);
+const scl=.1;
+const shiftX=Vector2(265,110)*scl;
+const shiftY=Vector2(0,-145)*scl;
+const shiftZ=Vector2(-120,220)*scl;
 
 var origin=Vector2(200,200);
 var blockTemplate=[];
@@ -13,9 +14,9 @@ func _ready():
 	
 func addBlock(type,pX,pY,pZ):
 	var a=blockTemplate[type].instance();
-	a.scale=Vector2(.2,.2);
-	a.position=origin+shiftX*pX+shiftY*pY+shiftZ*pZ;
+	a.scale=Vector2(scl,scl);
 	self.add_child(a);
+	blockInstance.append([a,pX,pY,pZ]);
 
 func genLvl(filename):
 	var fid=File.new();
@@ -31,3 +32,30 @@ func genLvl(filename):
 				for y in range(int(a[2]),int(a[5])+1):
 					for z in range(int(a[3]),int(a[6])+1):
 						addBlock(int(a[0]),x,y,z);
+	blockInstance.sort_custom(self,"sortBlock");
+	for i in range(len(blockInstance)):
+		#only 10 dudes can stand on a single tile before they start glitching
+		blockInstance[i][0].z_index=i;
+	updateBlockPos();
+
+func sortBlock(a,b):
+	if a[2]!=b[2]:
+		return a[2]<b[2];
+	if a[3]!=b[3]:
+		return a[3]<b[3];
+	return a[1]<b[1];
+
+func updateBlockPos():
+	for a in blockInstance:
+		a[0].position=shiftX*a[1]+shiftY*a[2]+shiftZ*a[3]+origin;
+
+func _process(dt):
+	if Input.is_action_pressed("ui_left"):
+		origin[0]+=dt*100;
+	if Input.is_action_pressed("ui_right"):
+		origin[0]-=dt*100;
+	if Input.is_action_pressed("ui_up"):
+		origin[1]+=dt*100;
+	if Input.is_action_pressed("ui_down"):
+		origin[1]-=dt*100;
+	updateBlockPos();
