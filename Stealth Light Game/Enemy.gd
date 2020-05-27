95289e20a8442505a1e8111be83ffd246f3d956a
doublebman13 <doublebman13@gmail.com>
2020-05-26 23:58:14 -0400

    Skeleton of the detection code and behaviors

--- Stealth Light Game/Enemy.gd
+++ Stealth Light Game/Enemy.gd
@@ -5,32 +5,49 @@ enum State {RELAXED,SUS,ALERT}
 var _state = State.RELAXED
 var direction = Vector2()
 
+const time_sus = 5
+const time_alert = 20
+var time = 0
+
 const angle = 20
 onready var vision_cone = $VisionCone
-
+var vision_distance = 20
 
 func _process(_delta):
-	#if player_collision hits first enemy_collision
-		#_state = State.SUS
-	#if player_collision hits second enemy_collision
-		#_state = State.ALERT
-		
-		#BUT HOW TO CODE THIS
+	for i in range(deg2rad(-angle),deg2rad(angle)):
+		pass
+		#sweep in front of enemy over i with vision_distance x 
+		#if vision_cone hits player and _state == State.RELAXED:
+			#_state = State.SUS
+			#distance = x+y
+		#elif vision_cone hits player and _state == State.SUS:
+			#_state = State.ALERT
 		
 	if _state == State.RELAXED:
 		pass
+		#walk predetermined path
 	elif _state == State.SUS:
 		pass
-		#for some amount of time
+		#for time < time_sus:
 			#don't change speed but follow the player for a bit
+			#if player is still in sus range
+				#time = 0
+			#else
+				#time += delta
+		time = 0
+		_state = State.RELAXED
+		#vision_distance = x
 	elif _state == State.ALERT:
 		pass
-		#aggressive action/attack the player
-		#speed up and follow the player
-		#for some amount of time after line of sight breaks
-			#look for the player based on available pathways
+		#for time < time_alert:
+			#aggressive action/attack the player
+			#speed up and follow the player
+			#if player is still in alert range
+				#time = 0
+			#else:
+				#time += delta
+		time = 0
+		_state = State.SUS
 	else:
 		_state = State.RELAXED
 	
-	for i in range(-angle,angle):
-		pass
