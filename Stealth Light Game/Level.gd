f6b20f45b5c75134caa4a3dc47880255e78a3cb1
Doots <65699806+Dootsu@users.noreply.github.com>
2020-05-27 10:23:07 -0400

    Wall but broken
    Walls are added, and level text file is commented
    walls kinda fold into themselves though, gotta fix that

--- Stealth Light Game/Level.gd
+++ Stealth Light Game/Level.gd
@@ -1,19 +1,19 @@
 extends Node2D
 
 const scl=.1;
-const shiftX=(400)*scl;
-const shiftY=(1)*scl;
-const shiftZ=(400)*scl;
+const shiftFlat=(400);
+const shiftUp=(1)*scl;
 
-var FLOORTILE;
 var TEXTURES;
 var floors;
+var walls;
 var tileInstance;
+
+var temp;
 
 var origin;
 
 func _ready():
-	FLOORTILE=preload("res://FloorTile.tscn");
 	TEXTURES=[
 		preload("res://tex/floor0.tres"),
 		preload("res://tex/floor1.tres"),
@@ -22,16 +22,37 @@ func _ready():
 	origin=get_viewport().size/2;
 	genLvl("lvl0.txt");
 
-func addBlock(type,pX,pY,pZ):
-	var a=FLOORTILE.instance();
-	a.scale=Vector2(scl,scl);
-	a.position=Vector2(pX*shiftX,pZ*shiftZ);
+func addFloor(type,pX,pY,pZ):
+	var a=Sprite.new();
 	a.set_texture(TEXTURES[type]);
+	a.scale=Vector2(scl,scl);
+	a.position=Vector2(pX*shiftFlat*scl,pZ*shiftFlat*scl);
 	floors[pY].add_child(a);
 	tileInstance.append([a,pX,pY,pZ]);
 
+func addWall(type,pX,pY,pZ,o):
+	var a=Polygon2D.new();
+	a.set_texture(TEXTURES[type]);
+	var c=shiftFlat*.5*scl;
+	var b;
+	b=PoolVector2Array();
+	b.append(Vector2(-1 if o<5 else 1,-1 if o<3 or o>6 else 1)*c);
+	b.append(Vector2(-1 if o<8 and o>3 else 1,-1 if o<6 and o>1 else 1)*c);
+	b.append(Vector2(0,0));
+	b.append(Vector2(0,0));
+	a.set_polygon(b);
+	b=PoolVector2Array();
+	b.append(Vector2(0,0));
+	b.append(Vector2(0,shiftFlat));
+	b.append(Vector2(shiftFlat,shiftFlat));
+	b.append(Vector2(shiftFlat,0));
+	a.set_uv(b);
+	a.position=Vector2(pX*shiftFlat*scl,pZ*shiftFlat*scl);
+	walls[pY].add_child(a);
+	temp=a;
+	
 func genLvl(filename):
-	floors=[];tileInstance=[];
+	floors=[];walls=[];tileInstance=[];
 	var fid=File.new();
 	fid.open("res://lvl/"+filename,File.READ);
 	while not fid.eof_reached():
@@ -45,14 +66,24 @@ func genLvl(filename):
 				for y in range(int(a[3]),int(a[6])+1):
 					for z in range(int(a[4]),int(a[7])+1):
 						while(len(floors)<=y):
-							floors.append(Node2D.new());
-							floors[-1].z_index=2*len(floors);
-							self.add_child(floors[-1]);
-						addBlock(int(a[1]),x,y,z);
+							floors.append(Node2D.new());walls.append(Node2D.new());
+							floors[-1].z_index=2*len(floors)-1;walls[-1].z_index=2*len(walls);
+							self.add_child(floors[-1]);self.add_child(walls[-1]);
+						if int(a[0]):
+							addWall(int(a[1]),x,y,z,int(a[0]));
+						else:
+							addFloor(int(a[1]),x,y,z);
 
 func shiftFloors():
+	var s=self.position*shiftUp;
 	for i in range(len(floors)):
-		floors[i].position=self.position*shiftY*i+origin;
+		floors[i].position=s*i+origin;
+		walls[i].position=s*i+origin;
+		for w in walls[i].get_children():
+			var a=w.get_polygon();
+			a.set(2,a[1]+s);
+			a.set(3,a[0]+s);
+			temp.set_polygon(a);
 
 func _process(dt):
 	if Input.is_action_pressed("ui_left"):
