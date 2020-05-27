fba29d63da7715c1098dda46759c4f2c447f20a6
whatislostinthemines <whatislostinthemines@gmail.com>
2020-05-26 23:15:16 -0400

    new movement system
    there's now acceleration for turning, how magical

--- Stealth Light Game/src/Player.gd
+++ Stealth Light Game/src/Player.gd
@@ -1,11 +1,15 @@
 extends KinematicBody2D
 signal death
 
-const ACCEL = 0.33
+const ACCEL = 0.2
+const DEACCEL = 0.2
+const TURNACCEL = 0.33
 const MAXSPEED = 250
 const HEALSPEED = 0.033
+const GENEROUS = 5
 #these values all relate to movement, subject to change
 var maxSpeedActual = MAXSPEED
+var impulse = Vector2()
 var direction = Vector2()
 var velocity = Vector2()
 var speed = 0
@@ -36,33 +40,43 @@ func _physics_process(delta):
 	if dead:
 		speed = lerp(speed, 0, ACCEL)
 	else:
-		get_input()
+		get_impulse()
 	#deterime speed
 	#move
-	move_and_slide(velocity*speed)
+	move_and_slide(velocity)
 	
 	
 
 #get the direction+speed
-func get_input():
-	direction = Vector2()
+func get_impulse():
+	impulse = Vector2(0,0)
 	#x axis
 	if Input.is_action_pressed("ui_right"):
-		direction.x += 1
+		impulse.x += 1
 	if Input.is_action_pressed("ui_left"):
-		direction.x -= 1
+		impulse.x -= 1
 	#y axis
 	if Input.is_action_pressed("ui_up"):
-		direction.y -= 1
+		impulse.y -= 1
 	if Input.is_action_pressed("ui_down"):
-		direction.y += 1
-	#check if input was applied, apply speed if so
-	#otherwise, use previous direction and apply deacceleration
-	if direction.length_squared() > 0:
-		speed = lerp(speed, maxSpeedActual, ACCEL)
-		velocity = direction.normalized()
+		impulse.y += 1
+	#some bullshit right here
+	
+	impulse = impulse.normalized()
+	if impulse.length_squared() > 0:
+		#below value exists because it takes a bit for velocity.length() to reach zero, 
+		#if so direction immediately equals impulse
+		if velocity.length() < GENEROUS:
+			direction = impulse
+		#no? then apply turn acceleration
+		else:
+			direction = direction.linear_interpolate(impulse, TURNACCEL)
+		speed = lerp(speed, MAXSPEED, ACCEL)
+	#no impulse means slow down
 	else:
-		speed = lerp(speed, 0, ACCEL)
+		speed = lerp(speed, 0, DEACCEL)
+	#finish by applying velocity
+	velocity = direction*speed
 
 
 
