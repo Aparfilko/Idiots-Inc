6f52e3de63003c7e1acf39b178e0220a1c0bda94
whatislostinthemines <whatislostinthemines@gmail.com>
2020-05-28 15:32:10 -0400

    no this is better
    ah

--- Stealth Light Game/src/Player.gd
+++ Stealth Light Game/src/Player.gd
@@ -1,11 +1,10 @@
 extends KinematicBody2D
 signal death
 #movement values, deprecated for now
-#const ACCEL = 0.2
-#const DEACCEL = 0.2
-#const TURNACCEL = 0.33
-#const MAXSPEED = 200
-#var speed = 0
+const ACCEL = 0.2
+const DEACCEL = 0.2
+const TURNACCEL = 0.33
+const MAXSPEED = 200
 const HEALSPEED = 0.033
 const GENEROUS = 5
 #health values
@@ -13,6 +12,11 @@ var light = false
 #this is also the transparency
 var health = 100
 var dead = false
+var speed = 0
+var velocity = Vector2()
+var direction = Vector2()
+var impulse
+var maxSpeedActual
 
 func _ready():
 	pass
@@ -47,32 +51,31 @@ func lit_up():
 
 
 func get_impulse():
-#	impulse = Vector2(0,0)
-#	#x axis
-#	if Input.is_action_pressed("ui_right"):
-#		impulse.x += 1
-#	if Input.is_action_pressed("ui_left"):
-#		impulse.x -= 1
-#	#y axis
-#	if Input.is_action_pressed("ui_up"):
-#		impulse.y -= 1
-#	if Input.is_action_pressed("ui_down"):
-#		impulse.y += 1
-#	#some bullshit right here
-#
-#	impulse = impulse.normalized()
-#	if impulse.length_squared() > 0:
-#		#below value exists because it takes a bit for velocity.length() to reach zero, 
-#		#if so direction immediately equals impulse
-#		if velocity.length_squared() < GENEROUS:
-#			direction = impulse
-#		#no? then apply turn acceleration
-#		else:
-#			direction = direction.linear_interpolate(impulse, TURNACCEL)
-#		speed = lerp(speed, MAXSPEED, ACCEL)
-#	#no impulse means slow down
-#	else:
-#		speed = lerp(speed, 0, DEACCEL)
-#	#finish by applying velocity
-#	velocity = direction*speed
-#		pass
+	impulse = Vector2(0,0)
+	#x axis
+	if Input.is_action_pressed("ui_right"):
+		impulse.x += 1
+	if Input.is_action_pressed("ui_left"):
+		impulse.x -= 1
+	#y axis
+	if Input.is_action_pressed("ui_up"):
+		impulse.y -= 1
+	if Input.is_action_pressed("ui_down"):
+		impulse.y += 1
+	#some bullshit right here
+
+	impulse = impulse.normalized()
+	if impulse.length_squared() > 0:
+		#below value exists because it takes a bit for velocity.length() to reach zero, 
+		#if so direction immediately equals impulse
+		if velocity.length_squared() < GENEROUS:
+			direction = impulse
+		#no? then apply turn acceleration
+		else:
+			direction = direction.linear_interpolate(impulse, TURNACCEL)
+		speed = lerp(speed, MAXSPEED, ACCEL)
+	#no impulse means slow down
+	else:
+		speed = lerp(speed, 0, DEACCEL)
+	#finish by applying velocity
+	velocity = direction*speed
