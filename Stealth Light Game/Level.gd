b332af97a02170a4df2f67bbbce212e4754a3c2d
Doots <65699806+Dootsu@users.noreply.github.com>
2020-05-29 15:24:16 -0400

    Wall fixed 2/2
    Shifted everything and aligned the top of the walls to the higher floor. Also theres a ghost enemy floating around, pace around and you'll hit them o.o

--- Stealth Light Game/Level.gd
+++ Stealth Light Game/Level.gd
@@ -1,8 +1,8 @@
 extends Node2D
 
-const scl=.1;
-const shiftFlat=(400);
-const shiftUp=(1)*scl;
+const sclFlat=.1;
+const sclUp=1.1;
+const shiftFlat=400;
 const shiftSpeed = 200;
 
 var TEXTURES;
@@ -12,8 +12,6 @@ var CANBELIT;
 var floors;
 var walls;
 var impulse;
-
-var origin;
 
 var lightTemp;
 var lightTempT1=0;
@@ -34,7 +32,7 @@ func _ready():
 	];
 	LIGHTTEX=preload("res://tex/light.tres");
 	CANBELIT=preload("res://tex/canBeLit.tres");
-	origin=get_viewport().size/2;
+	self.position=get_viewport().size/2;
 	genLvl("lvl1.txt");
 	addLight(0,0,0);
 
@@ -43,16 +41,15 @@ func addLight(pX,pY,pZ):
 	a.set_texture(LIGHTTEX);
 	a.mode=Light2D.MODE_MIX;
 	a.shadow_enabled=true;
-	a.position=Vector2(pX*shiftFlat*scl,pZ*shiftFlat*scl)
-	a.scale=Vector2(1,1)/scl;
+	a.position=Vector2(pX*shiftFlat*sclFlat,pZ*shiftFlat*sclFlat)
+	a.scale=Vector2(1,1)/sclFlat;
 	floors[pY].add_child(a);
 	lightTemp=a;
 
 func addFloor(type,pX,pY,pZ):
 	var a=Sprite.new();
 	a.set_texture(GHOSTTEX[type]);
-	#a.scale=Vector2(scl,scl);
-	a.position=Vector2(pX*shiftFlat,pZ*shiftFlat);
+	a.position=Vector2(pX,pZ)*shiftFlat;
 	var g=Sprite.new();
 	g.set_texture(TEXTURES[type]);
 	g.set_material(CANBELIT);
@@ -60,23 +57,22 @@ func addFloor(type,pX,pY,pZ):
 	floors[pY].add_child(a);
 
 func addWall(type,pX,pY,pZ,o):
-	var c=shiftFlat*.5*scl;
 	var a=Polygon2D.new();
 	a.set_texture(GHOSTTEX[type]);
 	var b;
 	b=PoolVector2Array();
-	b.append(Vector2(-1 if o<5 else 1,-1 if o<3 or o>6 else 1)*c);
-	b.append(Vector2(-1 if o<8 and o>3 else 1,-1 if o<6 and o>1 else 1)*c);
+	b.append(Vector2(-1 if o<5 else 1,-1 if o<3 or o>6 else 1)*shiftFlat*.5);
+	b.append(Vector2(-1 if o<8 and o>3 else 1,-1 if o<6 and o>1 else 1)*shiftFlat*.5);
 	b.append(Vector2(0,0));
 	b.append(Vector2(0,0));
 	a.set_polygon(b);
 	b=PoolVector2Array();
-	b.append(Vector2(0,0));
-	b.append(Vector2(0,shiftFlat));
-	b.append(Vector2(shiftFlat,shiftFlat));
-	b.append(Vector2(shiftFlat,0));
+	b.append(Vector2(0,0)*shiftFlat);
+	b.append(Vector2(0,1)*shiftFlat);
+	b.append(Vector2(1,1)*shiftFlat);
+	b.append(Vector2(1,0)*shiftFlat);
 	a.set_uv(b);
-	a.position=Vector2(pX*shiftFlat*scl,pZ*shiftFlat*scl);
+	a.position=Vector2(pX,pZ)*shiftFlat;
 	var g=Polygon2D.new();
 	g.set_texture(TEXTURES[type]);
 	g.set_material(CANBELIT);
@@ -87,8 +83,8 @@ func addWall(type,pX,pY,pZ,o):
 	var d=LightOccluder2D.new();
 	d.occluder=OccluderPolygon2D.new();
 	b=PoolVector2Array();
-	b.append(Vector2(-1 if o<5 else 1,-1 if o<3 or o>6 else 1)*c);
-	b.append(Vector2(-1 if o<8 and o>3 else 1,-1 if o<6 and o>1 else 1)*c);
+	b.append(Vector2(-1 if o<5 else 1,-1 if o<3 or o>6 else 1)*shiftFlat*.5);
+	b.append(Vector2(-1 if o<8 and o>3 else 1,-1 if o<6 and o>1 else 1)*shiftFlat*.5);
 	d.occluder.set_polygon(b);
 	a.add_child(d);
 	
@@ -110,7 +106,8 @@ func genLvl(filename):
 						while(len(floors)<=y):
 							floors.append(Node2D.new());walls.append(Node2D.new());
 							floors[-1].z_index=2*len(floors)-1;walls[-1].z_index=2*len(walls);
-							floors[-1].scale=Vector2(1,1)*scl*pow(1.09,len(floors)-1);
+							floors[-1].scale=Vector2(1,1)*sclFlat*pow(sclUp,len(floors)-1);
+							walls[-1].scale=Vector2(1,1)*sclFlat*pow(sclUp,len(floors)-1);
 							self.add_child(floors[-1]);self.add_child(walls[-1]);
 						if int(a[0]):
 							addWall(int(a[1]),x,y,z,int(a[0]));
@@ -119,12 +116,12 @@ func genLvl(filename):
 
 func shiftFloors():
 	for i in range(len(floors)):
-		floors[i].position=self.position*shiftUp*i+origin;
-		walls[i].position=self.position*shiftUp*i+origin;
+		floors[i].position=(self.position-get_viewport().size/2)*(pow(sclUp,i)-1);
+		walls[i].position=(self.position-get_viewport().size/2)*(pow(sclUp,i)-1);
 		for w in walls[i].get_children():
 			var a=w.get_polygon();
-			a.set(2,a[1]+(self.position+w.position)*shiftUp);
-			a.set(3,a[0]+(self.position+w.position)*shiftUp);
+			a.set(2,a[1]+((self.position-get_viewport().size/2)+(walls[i].position+w.position+a[1])*sclFlat)*pow(sclUp,i+1));
+			a.set(3,a[0]+((self.position-get_viewport().size/2)+(walls[i].position+w.position+a[0])*sclFlat)*pow(sclUp,i+1));
 			w.set_polygon(a);
 			w.get_child(0).set_polygon(w.get_polygon());
 
@@ -150,5 +147,5 @@ func _process(dt):
 	lightTempV2=clamp(lightTempV2+(randf()-.5)*.01,-5,5);
 	lightTempT1+=lightTempV1*dt;
 	lightTempT2+=lightTempV2*dt;
-	lightTemp.position=Vector2(shiftFlat/scl*sin(lightTempT1),shiftFlat/scl*sin(lightTempT2));
+	lightTemp.position=lightTemp.scale*shiftFlat*Vector2(sin(lightTempT1),sin(lightTempT2));
 	
