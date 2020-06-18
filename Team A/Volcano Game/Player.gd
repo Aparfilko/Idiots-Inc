b4524f7da38830a8ecdb86ee142b26e8f24d5624
whatislostinthemines <whatislostinthemines@gmail.com>
2020-06-18 17:02:24 -0400

    jump and gravity now working
    variable jump depending on how long you press space

--- Team A/Volcano Game/Player.gd
+++ Team A/Volcano Game/Player.gd
@@ -2,14 +2,20 @@ extends KinematicBody
 onready var sensitivity = 0.1
 onready var camera_angle = Vector2()
 
-var direction
-var velocity
+onready var direction = Vector3()
+onready var velocity = Vector3()
+const jump = 20
+const term = -120
+onready var jumpLow = 0.2
+onready var fall = 0.4
+onready var gravity = 2
+
 onready var speed = 0
-const MAXSPEED = 1200
+const MAXSPEED = 30
 const ACCEL = 0.1
 
 func _ready():
-	direction = Vector3()
+	pass
 
 #mouse movement
 func _input(event):
@@ -18,6 +24,8 @@ func _input(event):
 		var camera_change = Vector2(-event.relative * sensitivity)
 		self.rotate_y(deg2rad(camera_change.x))
 		camera_angle.x = (camera_angle.x + camera_change.x)
+		if camera_angle.x > 360:
+			camera_angle += -360
 		if camera_change.y + camera_angle.y < 90 and camera_change.y + camera_angle.y > -90:
 			$yHook.rotate_x(deg2rad(camera_change.y))
 			camera_angle.y += camera_change.y
@@ -25,11 +33,12 @@ func _input(event):
 #general movement
 func _physics_process(dt):
 	#get the movement wanted
-	get_impulse(dt)
+	get_impulse()
+	jumpy()
 	move_and_slide(velocity)
 	
 #
-func get_impulse(dt):
+func get_impulse():
 	var impulse = Vector2()
 	#x axis
 	if Input.is_action_pressed("ui_right"):
@@ -44,11 +53,20 @@ func get_impulse(dt):
 	impulse = impulse.normalized().rotated(deg2rad(-camera_angle.x))
 	if impulse.length_squared() > 0:
 		speed = lerp(speed, MAXSPEED, ACCEL)
-		direction = Vector3(impulse.x, 0, impulse.y)
+		direction = Vector3(impulse.x, direction.y, impulse.y)
 	else:
 		speed = lerp(speed, 0, ACCEL)
-	velocity = direction * speed * dt
+	velocity = Vector3(direction.x * speed , velocity.y, direction.z * speed)
 		
-	
-	
-
+func jumpy():
+	if velocity.y <= 0: #player is falling
+		if velocity.y > term and not is_on_wall():
+			velocity.y += -gravity * fall
+	elif velocity.y > 0: #jumping
+		if Input.is_action_pressed("ui_select"):
+			velocity.y += -gravity * jumpLow
+		else:
+			velocity.y += -gravity * fall
+	if Input.is_action_just_pressed("ui_select"):
+		if (is_on_wall()):
+			velocity.y = jump
