f3fc76b96fc15703579b8081d54dee36c043130a
Minerette <anjali.jothi@gmail.com>
2020-06-23 22:25:24 -0400

    Functional Pause Screen
    I have created a working pause screen

--- Team A/Volcano Game/Main.gd
+++ Team A/Volcano Game/Main.gd
@@ -7,16 +7,11 @@ func _ready():
 	var cameraBase = $"Test Person".camera_angle
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 
-#quit by pressing escape, temp until we have a pause menu
-func _input(event):
-	if Input.is_action_just_pressed("ui_cancel"):
-		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
-		get_tree().quit()
-	if Input.is_action_just_pressed("ui_reset"):		
-		get_tree().change_scene("res://Game.tscn")
-
 
 func _on_DeathZone_body_entered(body):
 	if body.name == "Test Person":
 		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 		get_tree().change_scene("res://ENDSCREN.tscn")
+
+
+
