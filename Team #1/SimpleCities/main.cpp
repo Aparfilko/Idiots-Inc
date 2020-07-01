bc2da58ddb78bc093db0fa55dc73fb8cefa58773
Doots <65699806+Dootsu@users.noreply.github.com>
2020-07-01 02:14:09 -0400

    house+office+road placement
    you want it weve got it

--- Team #1/SimpleCities/main.cpp
+++ Team #1/SimpleCities/main.cpp
@@ -14,21 +14,18 @@
 #include <vector>
 
 #define DIM 5
-#define MATSNUM 4
-#define CUBESNUM 7
+#define MATSNUM 7
+#define CUBESNUM 1
 
 namespace godot{
 class GDMain:public Spatial{
 	GODOT_CLASS(GDMain,Spatial)
 	
-	void initMeshPart(MeshInstance* &in,int cube,Vector3 pos=Vector3(0,0,0),Vector3 rot=Vector3(0,0,0)){
-		add_child(in=MeshInstance::_new());
-		in->set_mesh(cubes[cube]);
-		in->set_translation(pos);
-		in->set_rotation(rot);
-	}
+	typedef struct {char type,color;}mapTile;
+	typedef struct {int x,y;} carPathNode;
+	typedef struct {char color;std::vector<carPathNode> p;} carPath;
 	
-	SpatialMaterial* mats[MATSNUM];//grass,road,roadCurb,building0
+	SpatialMaterial* mats[MATSNUM];//gray,red,orange,yellow,green,blue,purple
 	CubeMesh* cubes[CUBESNUM];//floorTile,roadCenter,roadCorner,roadCurbCenter,roadCurbCorner,buildingMain,buildingConn
 	
 	ResourceLoader* rl;
@@ -38,8 +35,51 @@ class GDMain:public Spatial{
 	Input* input;
 	Camera* cam;
 	std::vector<Spatial*> foundations;
-	std::vector<Spatial*> houses;
-	std::vector<Spatial*> offices;
+	std::vector<MeshInstance*> buildings;
+	std::vector<MeshInstance*> roads;
+	int xDim,yDim;
+	std::vector<mapTile> lvl;
+	std::vector<carPath> paths;
+	
+	void addHouse(int x,int y,int c){
+		buildings.push_back((MeshInstance*)housePacked->instance());
+		buildings.back()->set_surface_material(0,mats[c]);
+		buildings.back()->set_translation(Vector3(x,.4,y));
+		buildings.back()->set_rotation(Vector3(0,-1.57,0));
+		add_child(buildings.back());
+		lvl[xDim*y+x].type=2;
+		lvl[xDim*y+x].color=c;
+	}
+	
+	void addOffice(int x,int y,int c){
+		buildings.push_back((MeshInstance*)officePacked->instance());
+		buildings.back()->set_surface_material(0,mats[c]);
+		buildings.back()->set_translation(Vector3(x,1.2,y));
+		buildings.back()->set_rotation(Vector3(0,-1.57,0));
+		add_child(buildings.back());
+		lvl[xDim*y+x].type=3;
+		lvl[xDim*y+x].color=c;
+	}
+	
+	void addRoad(int x,int y){
+		roads.push_back(MeshInstance::_new());
+		roads.back()->set_mesh(cubes[0]);
+		roads.back()->set_translation(Vector3(x,0,y));
+		add_child(roads.back());
+	}
+	
+	void placeRoads(){
+		addRoad(10,10);
+	}
+	
+	void printMap(){
+		for(int y=0;y<yDim;y++){
+			for(int x=0;x<xDim;x++){
+				std::cout<<(int)lvl[y*xDim+x].type<<" ";
+			}
+			std::cout<<std::endl;
+		}
+	}
 	
 	public:
 	GDMain(){}
@@ -48,48 +88,37 @@ class GDMain:public Spatial{
 	void _init(){
 		rl=ResourceLoader::get_singleton();
 		foundationPacked=rl->load("res://foundation.tscn");
-		housePacked=rl->load("res://model/house.glb");
-		officePacked=rl->load("res://model/office.glb");
+		housePacked=rl->load("res://house.tscn");
+		officePacked=rl->load("res://office.tscn");
 		
 		input=Input::get_singleton();
 		add_child(cam=Camera::_new());
 		cam->set_rotation(Vector3(-1.57,0,0));
-		cam->set_translation(Vector3(0,5,0));
 		
 		{
 		for(int i=0;i<MATSNUM;i++){mats[i]=SpatialMaterial::_new();}
-		mats[0]->set_albedo(Color(.2,.7,.4));
-		mats[1]->set_albedo(Color(.2,.2,.6));
-		mats[2]->set_albedo(Color(.9,.4,.2));
-		mats[3]->set_albedo(Color(.0,.4,.9));
+		mats[0]->set_albedo(Color(.5,.5,.5));
+		mats[1]->set_albedo(Color(1,.5,.5));
+		mats[2]->set_albedo(Color(1,.5,.3));
+		mats[3]->set_albedo(Color(1,.7,.3));
+		mats[4]->set_albedo(Color(.5,.8,.5));
+		mats[5]->set_albedo(Color(.5,.5,.8));
+		mats[6]->set_albedo(Color(.7,.5,.7));
 		}{
 		for(int i=0;i<CUBESNUM;i++){cubes[i]=CubeMesh::_new();}
-		cubes[0]->set_size(Vector3(.98,1,.98));
+		cubes[0]->set_size(Vector3(.5,.05,.5));
 		cubes[0]->set_material(mats[0]);
-		cubes[1]->set_size(Vector3(.8,1.2,.2));
-		cubes[1]->set_material(mats[1]);
-		cubes[2]->set_size(Vector3(.2,1.2,.2));
-		cubes[2]->set_material(mats[1]);
-		cubes[3]->set_size(Vector3(.8,1.3,.02));
-		cubes[3]->set_material(mats[2]);
-		cubes[4]->set_size(Vector3(.2,1.3,.2));
-		cubes[4]->set_material(mats[2]);
-		cubes[5]->set_size(Vector3(.8,1,.8));
-		cubes[5]->set_material(mats[3]);
-		cubes[6]->set_size(Vector3(.7,1,.7));
-		cubes[6]->set_material(mats[3]);
 		}
 		
-		FILE* fid=fopen("lvl0.cty","r");
+		FILE* fid=fopen("lvl1.cty","r");
 		char line[256];
-		int xDim,yDim;
 		fgets(line,256,fid);
 		sscanf(line,"%d%d",&xDim,&yDim);
-		cam->set_translation(Vector3(xDim,5,yDim));
+		cam->set_translation(Vector3(xDim,10,yDim));
 		fgets(line,256,fid);
 		sscanf(line,"%d%d",&xDim,&yDim);
-		int lvl[yDim*xDim]{};
-		
+		lvl.resize(xDim*yDim);
+		for(int i=0;i<lvl.size();i++){lvl[i].type=0;}
 		while(fgets(line,256,fid)){
 			if(line[0]=='\n'||line[0]=='#'){continue;}
 			if(line[0]=='='){break;}
@@ -100,15 +129,30 @@ class GDMain:public Spatial{
 			
 			for(int y=b;y<=d;y++){
 				for(int x=a;x<=c;x++){
-					lvl[y*xDim+x]=1;
+					lvl[y*xDim+x].type=1;
 					foundations.push_back((Spatial*)foundationPacked->instance());
 					foundations.back()->set_translation(Vector3(x,0,y));
 					add_child(foundations.back());
 				}
 			}
-			
 		}
 		fclose(fid);
+		
+		addHouse(1,1,1);
+		addHouse(18,6,1);
+		addHouse(18,18,1);
+		addHouse(1,13,1);
+		addOffice(18,10,1);
+		
+		addHouse(3,1,5);
+		addHouse(15,7,5);
+		addHouse(15,18,5);
+		addHouse(1,11,5);
+		addOffice(7,1,5);
+		
+		placeRoads();
+		
+		printMap();
 	}
 	
 	void _input(InputEvent* in){
