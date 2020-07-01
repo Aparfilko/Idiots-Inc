ebacdb3fbb372a9e11ad376227e6a6b1b6e6c5a0
doublebman13 <doublebman13@gmail.com>
2020-06-30 23:49:42 -0400

    Trying to make individual clicks on the foundations work

--- Team #1/SimpleCities/CollisionShape.gd
+++ Team #1/SimpleCities/CollisionShape.gd
@@ -1,7 +1,18 @@
 extends CollisionShape
 
+
 func _ready():
 	set_process_input(true)
+	
+func get_object_under_mouse():
+	var mouse_pos = get_viewport().get_mouse_position()
+	var ray_from = get_parent().get_parent().get_parent().get_child(0).project_ray_origin(mouse_pos)
+	var RAY_LENGTH=10
+	var ray_to = ray_from + get_parent().get_parent().get_parent().get_child(0).project_ray_normal(mouse_pos) * RAY_LENGTH
+	var space_state = get_world().direct_space_state
+	var selection = space_state.intersect_ray(ray_from, ray_to)
+	return selection
+	
 func _input(event):
-	if(event.type == InputEvent.MOUSE_BUTTON && event.button_index == BUTTON_LEFT):
-		print("Clicked!")
+	if event.is_action_pressed("left_click") and get_object_under_mouse().name_begins_with("CollisionShape"):
+		print("Pow")
