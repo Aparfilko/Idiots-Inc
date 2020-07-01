e193b608e83e5c302cb396eb1af2075ed6a5497d
Aparfilko <are.they.giants@gmail.com>
2020-07-01 05:06:13 -0400

    added demo spotlight

--- Team A/Simplify Game/World 1.gd
+++ Team A/Simplify Game/World 1.gd
@@ -3,11 +3,19 @@ extends Spatial
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-
-
+var spot = Vector3(1,0,0)
+var spotback = Vector3(-4,0,0)
+var count = 0
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 func _unhandled_input(event):
-	$SpotLight.visible = true
+	if event is InputEventKey:
+		if event.pressed and event.scancode == KEY_SPACE:
+			$SpotLight.visible = true
+			count += 1
+			$SpotLight.translate(spot)
+			if count > 2:
+				$SpotLight.translate(spotback)
+				count=-1
