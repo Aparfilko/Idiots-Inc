b10c1effcf8906ae3ab77c9d8d202f0a1031d988
Aparfilko <are.they.giants@gmail.com>
2020-07-01 23:43:59 -0400

    Revert "Merge branch 'UI_Branch_Cronchy' into Team-A"
    This reverts commit 7a4b4e1b9a89001126b421903f8860f40e7e508a, reversing
    changes made to c016c781849851b04d00ec54b5e12ecafa3ff7a0.

--- Team A/Simplify Game/select.gd
+++
@@ -1,9 +0,0 @@
-extends StaticBody2D
-signal clicked(node)
-
-func _ready():
-	connect("clicked", get_parent(), "_select_word")
-	
-func _input_event(viewport, event, shape_idx):
-	if Input.is_action_just_pressed("click"):
-		emit_signal("clicked", self)
