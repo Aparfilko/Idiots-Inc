f5b62fb9bd79242ef37c5d03a358a20afb7486b7
Aparfilko <are.they.giants@gmail.com>
2020-07-03 02:37:50 -0400

    tried to save this project

--- Team A/Simplify Game/scripts/pause menu.gd
+++ Team A/Simplify Game/scripts/pause menu.gd
@@ -16,3 +16,18 @@ func _input(_event):
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+func _on_Resume_pressed():
+	visible = false
+
+func _on_toMenu_pressed():
+	pass # Replace with function body.
+
+func _on_Mute_pressed():
+	if get_parent().get_parent().get_child(4).playing == true:
+		get_parent().get_parent().get_child(4).stop()
+		get_node("Panel/VBoxContainer/Mute").set_text("Play Music")
+	else:
+		get_parent().get_parent().get_child(4).play()
+		get_node("Panel/VBoxContainer/Mute").set_text("Mute Music")
+		
