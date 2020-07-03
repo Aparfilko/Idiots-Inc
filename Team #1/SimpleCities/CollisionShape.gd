84a3e8f98555d9edb47af44961d41d49ecfa2b75
doublebman13 <doublebman13@gmail.com>
2020-07-03 00:07:14 -0400

    Sends color information to the building placer function

--- Team #1/SimpleCities/CollisionShape.gd
+++ Team #1/SimpleCities/CollisionShape.gd
@@ -19,6 +19,21 @@ func get_object_under_mouse():
 		return false
 	
 func _input(event):
+	if event.is_action_pressed("0"):
+		color = 0
+	if event.is_action_pressed("1"):
+		color = 1
+	if event.is_action_pressed("2"):
+		color = 2
+	if event.is_action_pressed("3"):
+		color = 3
+	if event.is_action_pressed("4"):
+		color = 4
+	if event.is_action_pressed("5"):
+		color = 5
+	if event.is_action_pressed("6"):
+		color = 6
+	
 	if get_object_under_mouse() == true:
 		var x = get_parent().get_parent().get_translation().x
 		var z = get_parent().get_parent().get_translation().z
@@ -28,4 +43,4 @@ func _input(event):
 			type = 3
 		elif event.is_action_pressed("middle_click"):
 			type = 0
-		get_parent().get_parent().get_parent()._foundation_place_building(x,z,2,type)
+		get_parent().get_parent().get_parent()._foundation_place_building(x,z,color,type)
