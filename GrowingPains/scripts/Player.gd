95dedb4c3d3136cffc93a18c912b4235466a1d23
whatislostinthemines <62076774+whatislostinthemines@users.noreply.github.com>
2020-07-05 00:41:32 -0400

    player altered to fit into the world better

--- GrowingPains/scripts/Player.gd
+++ GrowingPains/scripts/Player.gd
@@ -3,13 +3,13 @@ extends KinematicBody2D
 
 const grav = 1000
 const accel = 0.5
-const bashLength = Vector2(100,0)
+const bashLength = Vector2(150,0)
 signal music(age)
 signal killed()
 onready var vel = Vector2()
 onready var age = 0
-onready var speed = [15000, 25000, 7500] 
-onready var jump = [20000, 25000, 10000]
+onready var speed = [15000, 25000, 10000]
+onready var jump = [24000, 32000, 10000]
 onready var pins = [Vector2(),Vector2(),Vector2()]
 
 var manager;
@@ -32,7 +32,7 @@ func _input(event):
 
 func _physics_process(dt):
 	get_movement(dt)
-	move_and_slide(vel, Vector2(0,-1))
+	move_and_slide(vel, Vector2.UP)
 	for i in get_slide_count():
 		if(get_slide_collision(i)["collider"].get_class()=="Level_Goal"):
 			manager.goal_reached();
@@ -60,6 +60,8 @@ func get_movement(dt):
 			vel.y = -jump[age] * dt
 	#if in air, then you must fall
 	else:
+		if is_on_ceiling():
+			vel.y = 0
 		vel.y += grav * dt
 	#bash raycast must face direction player is moving
 	if vel.x > 0:
