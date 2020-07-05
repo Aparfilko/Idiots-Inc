13c086a5dea6856cb76fed8ec3df6c2781918c37
Doots <65699806+Dootsu@users.noreply.github.com>
2020-07-05 02:56:15 -0400

    Level 4
    feast your eyes upon these colorful doors

--- GrowingPains/scripts/Door.gd
+++ GrowingPains/scripts/Door.gd
@@ -7,5 +7,5 @@ func _ready():
 
 func _switch(_body_id, node, _body_shape, _area_shape):
 	if node.name.match("Player"):
-		$CollisionShape2D.call_deferred("set", "disabled", not $CollisionShape2D.is_disabled())
-		$Sprite.call_deferred("set", "visible", not $Sprite.is_visible())
+		$CollisionShape2D.call_deferred("set", "disabled", true)
+		$Sprite.call_deferred("set", "visible", false)
--- GrowingPains/scripts/LevelManager.gd
+++ GrowingPains/scripts/LevelManager.gd
@@ -4,6 +4,7 @@ onready var lvls=[
 	preload("res://levels/Level1.tscn"),
 	preload("res://levels/Level2.tscn"),
 	preload("res://levels/Level3.tscn"),
+	preload("res://levels/Level4.tscn"),
 	];
 onready var a=0;
 var currLvl;
