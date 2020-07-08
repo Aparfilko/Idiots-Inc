db53cad48fff8c0cc0a05f070acb338d912d50a2
whatislostinthemines <whatislostinthemines@gmail.com>
2020-07-08 15:27:51 -0400

    added hotkeys+ ability to place lamps

--- LightEmUp/scripts/selector.gd
+++ LightEmUp/scripts/selector.gd
@@ -1,22 +1,28 @@
 extends TileMap
 
-enum {WALL = 0, ALLLAMP = 1,DOWNLAMP = 2, UPLAMP = 3, LEFTLAMP = 4, RIGHTLAMP = 5, NOSELECT = 6, SELECT = 7, NOSELECTW = 8}
+enum {WALL = 0, ALLLAMP = 1,DOWNLAMP = 2, UPLAMP = 3, LEFTLAMP = 4, RIGHTLAMP = 5, NOSELECT = 6, SELECT = 7, PLACE = 10, DELETE = 11}
 #m for mode
 onready var m = false
 var index
-onready var prev = [Vector2(0,0) ,get_cell(0,0)]
+onready var prev = [Vector2(-999,-999) ,get_cell(-999,-999), get_cell_autotile_coord(-999,-999)]
 var placeable
-onready var choose = ALLLAMP
+onready var choose = INVALID_CELL
+onready var select = false
 
 func _ready():
 	#connect so buttons do things
 	index = world_to_map(get_global_mouse_position())
-	get_parent().get_node("Camera2D/User Interface/toolbar/lamp").connect("button_down", self, "placeMode")
-	get_parent().get_node("Camera2D/User Interface/toolbar/lamp").connect("button_up", self, "noMode")
 	
 func _process(_dt):
-	if choose != INVALID_CELL:
-		placeable = hover(choose)
+	#canceled, go to normal
+	if choose == INVALID_CELL:
+		pass
+	#should have delete mode here
+	elif choose == NOSELECT:
+		select = delete()
+	#otherwise, hover with selection
+	else:
+		select = hover(choose)
 	
 	
 func hover(item):
@@ -25,26 +31,60 @@ func hover(item):
 	var free = get_parent().get_node("Floor").get_cellv(index) != INVALID_CELL
 	#revert previous tiles
 	if index != prev[0]:
-		set_cellv(prev[0], prev[1])
-		$Selection.set_cellv(prev[0], INVALID_CELL)
+		set_cell(prev[0].x, prev[0].y, prev[1], false, false, false, prev[2])
+		$Selection.set_cell(prev[0].x, prev[0].y, INVALID_CELL, false, false, false, prev[2])
 		#grab current tile
-		prev = [index, get_cellv(index)]
-	if item != INVALID_CELL and free:
+		prev = [index, get_cellv(index), get_cell_autotile_coord(index.x, index.y)]
+	if prev[1] != WALL and free:
 		#you're ok, change it
 		if prev[1] == INVALID_CELL:
-			$Selection.set_cellv(index, SELECT)
-			set_cellv(index, item)
+			$Selection.set_cell(index.x, index.y, SELECT,false,false,false,prev[2])
+			set_cell(index.x, index.y, item, false,false,false,prev[2])
 			return true
 		#something there, can't select
-		#this is here because I can't figure out how to keep selection from drawing underneath obs, whoops!
-		elif prev[1] == WALL:
-			$Selection.set_cellv(index, NOSELECTW)
 		else:
-			$Selection.set_cellv(index, NOSELECT)
+			$Selection.set_cell(index.x, index.y, NOSELECT,false,false,false,prev[2])
 	return false
 	
-func noMode():
-	m = WALL
+func delete():
+	index = get_parent().get_node("Floor").world_to_map(get_global_mouse_position())
+	#revert previous tiles
+	#don't select over nothing!
+	var free = get_parent().get_node("Floor").get_cellv(index) != INVALID_CELL
+	if index != prev[0]:
+		set_cell(prev[0].x, prev[0].y, prev[1], false, false, false, prev[2])
+		$Selection.set_cell(prev[0].x, prev[0].y, INVALID_CELL, false, false, false, prev[2])
+		prev = [index, get_cellv(index), get_cell_autotile_coord(index.x, index.y)]
+	if prev[1] != WALL and free:
+		#mark this as something to delete
+		$Selection.set_cell(index.x, index.y, NOSELECT,false,false,false,prev[2])
+		return true
+	return false
 	
-func placeMode():
-	m = ALLLAMP
+func revert():
+	set_cell(prev[0].x, prev[0].y, prev[1], false, false, false, prev[2])
+	$Selection.set_cell(prev[0].x, prev[0].y, INVALID_CELL, false, false, false, prev[2])
+	
+	
+func get_choice(c):
+	if c == INVALID_CELL:
+		choose = c
+		revert()
+	#place item
+	elif c == PLACE:
+		#don't let place happen if there's stuff already there
+		if select:
+			#delete tile
+			if choose == NOSELECT:
+				set_cell(prev[0].x, prev[0].y, INVALID_CELL, false, false, false, prev[2])
+			#place tile
+			$Selection.set_cell(prev[0].x, prev[0].y, INVALID_CELL, false, false, false, prev[2])
+			prev = [Vector2(-999,-999) ,get_cell(-999,-999), get_cell_autotile_coord(-999,-999)]
+	#switch back from delete mode
+	elif choose == NOSELECT and c == NOSELECT:
+		$Selection.set_cell(prev[0].x, prev[0].y, INVALID_CELL, false, false, false, prev[2])
+		prev = [Vector2(-999,-999) ,get_cell(-999,-999), get_cell_autotile_coord(-999,-999)]
+		choose = INVALID_CELL
+	#select item to place/go into delete mode
+	else:
+		choose = c
--- LightEmUp/scripts/ui/User Interface.gd
+++ LightEmUp/scripts/ui/User Interface.gd
@@ -1,16 +1,35 @@
 extends Control
+enum {NOTHING = -1, ALLLAMP = 1,DOWNLAMP = 2, LEFTLAMP = 3, UPLAMP = 4, RIGHTLAMP = 5, DELETE = 6, PLACE = 10}
+signal pass_choice(choice)
 
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	connect("pass_choice", get_parent().get_parent().get_node("Obs"), "get_choice")
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+#hotkeys
+func _input(event):
+	#press spacebar to cancel selection
+	if event.is_action("ui_select") and Input.is_action_just_pressed("ui_select"):
+		express_choice(NOTHING)
+	#1
+	if event.is_action("alllamp") and Input.is_action_just_pressed("alllamp"):
+		express_choice(ALLLAMP)
+	#2
+	if event.is_action("downlamp") and Input.is_action_just_pressed("downlamp"):
+		express_choice(DOWNLAMP)
+	#3
+	if event.is_action("uplamp") and Input.is_action_just_pressed("uplamp"):
+		express_choice(UPLAMP)
+	#4
+	if event.is_action("leftlamp") and Input.is_action_just_pressed("leftlamp"):
+		express_choice(LEFTLAMP)
+	#5
+	if event.is_action("rightlamp") and Input.is_action_just_pressed("rightlamp"):
+		express_choice(RIGHTLAMP)
+	#left click to place
+	if event.is_action("place") and Input.is_action_just_pressed("place"):
+		express_choice(PLACE)
+	#right click to activate delete mode
+	if event.is_action("delete") and Input.is_action_just_pressed("delete"):
+		express_choice(DELETE)
+func express_choice(tile):
+	emit_signal("pass_choice", tile)
