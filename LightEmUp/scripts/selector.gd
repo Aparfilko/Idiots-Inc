fd4591d5625b92ec95cf8836788efdc986ddc91f
whatislostinthemines <whatislostinthemines@gmail.com>
2020-07-08 13:44:08 -0400

    cleaned up selection, will next attempt to make this useable

--- LightEmUp/scripts/selector.gd
+++ LightEmUp/scripts/selector.gd
@@ -1,10 +1,12 @@
 extends TileMap
 
-enum {WALL = 0, ALLLAMP = 1,DOWNLAMP = 2, UPLAMP = 3, LEFTLAMP = 4, RIGHTLAMP = 5, NOSELECT = 6, SELECT = 7}
+enum {WALL = 0, ALLLAMP = 1,DOWNLAMP = 2, UPLAMP = 3, LEFTLAMP = 4, RIGHTLAMP = 5, NOSELECT = 6, SELECT = 7, NOSELECTW = 8}
 #m for mode
 onready var m = false
 var index
 onready var prev = [Vector2(0,0) ,get_cell(0,0)]
+var placeable
+onready var choose = ALLLAMP
 
 func _ready():
 	#connect so buttons do things
@@ -13,26 +15,33 @@ func _ready():
 	get_parent().get_node("Camera2D/User Interface/toolbar/lamp").connect("button_up", self, "noMode")
 	
 func _process(_dt):
+	if choose != INVALID_CELL:
+		placeable = hover(choose)
+	
+	
+func hover(item):
 	index = get_parent().get_node("Floor").world_to_map(get_global_mouse_position())
-#	don't select over nothing!
-	var free = get_parent().get_node("Floor").get_cell(index[0], index[1]) != INVALID_CELL
+	#don't select over nothing!
+	var free = get_parent().get_node("Floor").get_cellv(index) != INVALID_CELL
 	#revert previous tiles
 	if index != prev[0]:
 		set_cellv(prev[0], prev[1])
+		$Selection.set_cellv(prev[0], INVALID_CELL)
+		#grab current tile
 		prev = [index, get_cellv(index)]
-	#grab current tile
-	#print(index)
-	#print(prev)
-	#you're ok, change it
-	if prev[1] == INVALID_CELL and free:
-		set_cellv(index, SELECT)
-	#something there, can't select
-	elif free:
-		set_cellv(index, NOSELECT)
-		#print("no")
-	
-	
-	
+	if item != INVALID_CELL and free:
+		#you're ok, change it
+		if prev[1] == INVALID_CELL:
+			$Selection.set_cellv(index, SELECT)
+			set_cellv(index, item)
+			return true
+		#something there, can't select
+		#this is here because I can't figure out how to keep selection from drawing underneath obs, whoops!
+		elif prev[1] == WALL:
+			$Selection.set_cellv(index, NOSELECTW)
+		else:
+			$Selection.set_cellv(index, NOSELECT)
+	return false
 	
 func noMode():
 	m = WALL
