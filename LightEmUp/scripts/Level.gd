aae5a2979309e9e6be9b4eb3d2994d62e7a74525
Doots <65699806+Dootsu@users.noreply.github.com>
2020-07-08 17:08:06 -0400

    Light Engine Live Take 2
    fixed mostly

--- LightEmUp/scripts/Level.gd
+++ LightEmUp/scripts/Level.gd
@@ -9,12 +9,16 @@ func _ready():
 		$Floor.get_used_rect().position.y);
 	update_tiles();
 
+func print_tiles():
+	for i in $LightEngine.mat[1]:
+		print(i[0]);
+
 func update_tiles():
-	for i in $Floor.get_used_cells():
-		$LightEngine.set_mat(i.x,i.y,0,$Floor.get_cell(i.x,i.y),$Floor.get_cell_autotile_coord(i.x,i.y));
-	for i in $Obs.get_used_cells():
-		$LightEngine.set_mat(i.x,i.y,1,$Obs.get_cell(i.x,i.y),$Obs.get_cell_autotile_coord(i.x,i.y));
-	$LightEngine.update();
+	for y in range($LightEngine.yDim):
+		for x in range($LightEngine.xDim):
+			$LightEngine.set_mat(x,y,0,$Floor.get_cell(x,y),$Floor.get_cell_autotile_coord(x,y));
+			$LightEngine.set_mat(x,y,1,$Obs.get_cell(x,y),$Obs.get_cell_autotile_coord(x,y));
+		$LightEngine.update();
 	for i in $Floor.get_used_cells():
 		$Floor.set_cell(i.x,i.y,$LightEngine.get_mat(i.x,i.y,0,0),false,false,false,$LightEngine.get_mat(i.x,i.y,0,1));
 	for i in $Obs.get_used_cells():
@@ -31,4 +35,4 @@ func _input(event):
 	if event.is_action_pressed("ui_down"):
 		$Camera2D.position-=Vector2(-16,-32);
 	if event.is_action_pressed("ui_accept"):
-		print($LightEngine.mat[1]);
+		print(print_tiles());
