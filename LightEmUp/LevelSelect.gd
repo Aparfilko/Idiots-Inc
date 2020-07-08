3922def82a91f62e65a99f831c912c1fad13b5e2
doublebman13 <doublebman13@gmail.com>
2020-07-07 22:56:12 -0400

    Level Select ready to go

---
+++ LightEmUp/LevelSelect.gd
@@ -0,0 +1,50 @@
+extends Node2D
+
+
+func _on_1_pressed():
+	get_tree().change_scene('res://Levels/1.tscn')
+
+func _on_2_pressed():
+	get_tree().change_scene('res://Levels/2.tscn')
+
+func _on_3_pressed():
+	get_tree().change_scene('res://Levels/3.tscn')
+
+func _on_4_pressed():
+	get_tree().change_scene('res://Levels/4.tscn')
+
+func _on_5_pressed():
+	get_tree().change_scene('res://Levels/5.tscn')
+
+func _on_6_pressed():
+	get_tree().change_scene('res://Levels/6.tscn')
+
+func _on_7_pressed():
+	get_tree().change_scene('res://Levels/7.tscn')
+
+func _on_8_pressed():
+	get_tree().change_scene('res://Levels/8.tscn')
+
+func _on_9_pressed():
+	get_tree().change_scene('res://Levels/9.tscn')
+
+func _on_10_pressed():
+	get_tree().change_scene('res://Levels/10.tscn')
+
+func _on_11_pressed():
+	get_tree().change_scene('res://Levels/11.tscn')
+
+func _on_12_pressed():
+	get_tree().change_scene('res://Levels/12.tscn')
+
+func _on_13_pressed():
+	get_tree().change_scene('res://Levels/13.tscn')
+
+func _on_14_pressed():
+	get_tree().change_scene('res://Levels/14.tscn')
+
+func _on_15_pressed():
+	get_tree().change_scene('res://Levels/15.tscn')
+
+func _on_Back_pressed():
+	get_tree().change_scene('res://Title.tscn')
--- LightEmUp/Title.gd
+++ LightEmUp/Title.gd
@@ -2,7 +2,7 @@ extends Node2D
 
 
 func _on_Level_Select_pressed():
-	get_tree().change_scene('res://Level Select.tscn')
+	get_tree().change_scene('res://LevelSelect.tscn')
 
 func _on_Quit_pressed():
 	get_tree().quit()
