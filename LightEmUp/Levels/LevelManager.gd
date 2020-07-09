18ee8dc9b46eb76511e5379dc1e2a505f1e3187a
doublebman13 <doublebman13@gmail.com>
2020-07-08 22:23:09 -0400

    commented stuff out

--- LightEmUp/Levels/LevelManager.gd
+++ LightEmUp/Levels/LevelManager.gd
@@ -15,29 +15,29 @@ func load():
 	return content
 
 #array to run forward or back through levels
-onready var lvls=[
-	preload("res://Levels/Level1.tscn"),
-	preload("res://Levels/Level2.tscn"),
+#onready var lvls=[
+#	preload("res://Levels/Level1.tscn"),
+#	preload("res://Levels/Level2.tscn"),
 #	preload("res://Levels/Level3.tscn"),
 #	preload("res://Levels/Level4.tscn"),
 #	preload("res://Levels/Level5.tscn"),
 #	preload("res://Levels/Level6.tscn"),
 #	preload("res://Levels/Level7.tscn"),
 #	preload("res://Levels/LevelEndScreen.tscn")
-	];
-onready var a=0;
-var currLvl;
-
-func _ready():
-	updateLvl();
-
-func updateLvl():
-	if(currLvl):
-		currLvl.queue_free();
-	currLvl=lvls[a].instance();
-	currLvl.get_node("Player").set_manager(self);
-	add_child(currLvl);
-
-func choose_level(i):
-	a=a+i;
-	updateLvl();
+#	];
+#onready var a=0;
+#var currLvl;
+#
+#func _ready():
+#	updateLvl();
+#
+#func updateLvl():
+#	if(currLvl):
+#		currLvl.queue_free();
+#	currLvl=lvls[a].instance();
+#	currLvl.get_node("Player").set_manager(self);
+#	add_child(currLvl);
+#
+#func choose_level(i):
+#	a=a+i;
+#	updateLvl();
