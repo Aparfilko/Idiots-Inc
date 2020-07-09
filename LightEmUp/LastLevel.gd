a6d0af2d38c7c91e142c4b52173ca9c49146b94c
doublebman13 <doublebman13@gmail.com>
2020-07-08 22:04:42 -0400

    More Error

--- LightEmUp/LastLevel.gd
+++ LightEmUp/LastLevel.gd
@@ -3,4 +3,3 @@ extends Node2D
 
 func _on_Last_pressed():
 	pass
-#	choose_level(-1)
--- LightEmUp/NextLevel.gd
+++ LightEmUp/NextLevel.gd
@@ -2,5 +2,5 @@ extends Node2D
 
 
 func _on_Next_pressed():
+#	get_parent().get_parent().choose_level(1)
 	pass
-#	choose_level(1)
