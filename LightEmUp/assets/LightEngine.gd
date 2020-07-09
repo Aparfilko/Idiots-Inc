baa47051fe51623d0deac5c1fdae4483f09daf98
Doots <65699806+Dootsu@users.noreply.github.com>
2020-07-08 20:59:30 -0400

    Directional Lights
    they look

--- LightEmUp/assets/LightEngine.gd
+++ LightEmUp/assets/LightEngine.gd
@@ -30,7 +30,7 @@ func update():
 			if(mat[1][y0*xDim+x0][0]>0):
 				for y1 in range(yDim):
 					for x1 in range(xDim):
-						light[y1*xDim+x1]+=traceLight(x0,y0,x1,y1);
+						light[y1*xDim+x1]+=traceLight(mat[1][y0*xDim+x0][0],x0,y0,x1,y1);
 	
 	for y in range(yDim):
 		for x in range(xDim):
@@ -43,7 +43,16 @@ func update():
 			if(mat[1][ind][0]==0):
 				mat[1][ind][1]=Vector2(l*16+l*4+l,0);
 
-func traceLight(x0,y0,x1,y1):
+func traceLight(type,x0,y0,x1,y1):
+	if(type==2 and (y1>y0 or abs(x0-x1)>(y0-y1))):
+		return 0;
+	if(type==3 and (y1<y0 or abs(x0-x1)>(y1-y0))):
+		return 0;
+	if(type==4 and (x1>x0 or abs(y0-y1)>(x0-x1))):
+		return 0;
+	if(type==5 and (x1<x0 or abs(y0-y1)>(x1-x0))):
+		return 0;
+	
 	var out=1/pow((sqrt(pow(x1-x0,2)+pow(y1-y0,2))+4.5)/4.5,2);
 	while(x0!=x1 and y0!=y1):
 		if(abs(x1-x0)>abs(y1-y0)):
