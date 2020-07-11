1a9072038dbfbc54755dba35e4a751b4359f48da
whatislostinthemines <whatislostinthemines@gmail.com>
2020-07-11 16:02:48 -0400

    fixed small bug

--- Brakes Optional/script/ManagerLevel.gd
+++ Brakes Optional/script/ManagerLevel.gd
@@ -8,7 +8,7 @@ onready var resLevel=[
 ];
 
 func _ready():
-	refCar=get_parent().get_node("CAR");
+	refCar=get_parent().get_node("car");
 	refCurr=resLevel[0].instance();
 	add_child(refCurr);
 	refCar.transform=refCurr.get_node("SpawnPoint").transform;
