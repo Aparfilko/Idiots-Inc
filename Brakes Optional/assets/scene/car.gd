5729de627f55e7629f42c33f6fd72750ac0f8ef9
Doots <65699806+Dootsu@users.noreply.github.com>
2020-07-10 23:03:13 -0400

    a little bit more car control
    wont work yet

--- Brakes Optional/assets/scene/car.gd
+++ Brakes Optional/assets/scene/car.gd
@@ -8,21 +8,21 @@ var acc=0;
 var ang=0;
 var angVel=0;
 var angAcc=0;
-var boosters=[0,0,0,0,0,0];#ing,thrust,lev,left,brake,right
+var b=[0,0,0,0,0,0];#ing,thrust,lev,left,brake,right
 
 func _ready():
 	pass # Replace with function body.
 
 func _physics_process(delta):
-	translation+=vel;
+	vel+=Quat(Vector3(0,1,0),ang)*Vector3(0,0,(1 if b[1] else 0)-(1 if b[4] else 0));
+	
 
 func _input(event):
-	boosters=[
-		(boosters[0] or event.is_action_pressed("q")) and not event.is_action_released("q"),
-		(boosters[1] or event.is_action_pressed("w")) and not event.is_action_released("w"),
-		(boosters[2] or event.is_action_pressed("e")) and not event.is_action_released("e"),
-		(boosters[3] or event.is_action_pressed("a")) and not event.is_action_released("a"),
-		(boosters[4] or event.is_action_pressed("s")) and not event.is_action_released("s"),
-		(boosters[5] or event.is_action_pressed("d")) and not event.is_action_released("d"),
+	b=[
+		(b[0] or event.is_action_pressed("q")) and not event.is_action_released("q"),
+		(b[1] or event.is_action_pressed("w")) and not event.is_action_released("w"),
+		(b[2] or event.is_action_pressed("e")) and not event.is_action_released("e"),
+		(b[3] or event.is_action_pressed("a")) and not event.is_action_released("a"),
+		(b[4] or event.is_action_pressed("s")) and not event.is_action_released("s"),
+		(b[5] or event.is_action_pressed("d")) and not event.is_action_released("d"),
 		];
-	print(boosters);
