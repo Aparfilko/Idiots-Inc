23c8cdd0547d5a0ecb70a46f2a389b6ee796b119
Doots <65699806+Dootsu@users.noreply.github.com>
2020-07-10 20:14:00 -0400

    Camera works even better
    full wsad + flipped everything

--- Brakes Optional/script/car.gd
+++ Brakes Optional/script/car.gd
@@ -4,5 +4,11 @@ func _ready():
 	pass # Replace with function body.
 
 func _input(event):
-	if(event.is_action_pressed("ui_up")):
+	if(event.is_action_pressed("w")):
+		translate(Vector3(0,0,-1));
+	if(event.is_action_pressed("s")):
 		translate(Vector3(0,0,1));
+	if(event.is_action_pressed("a")):
+		translate(Vector3(-1,0,0));
+	if(event.is_action_pressed("d")):
+		translate(Vector3(1,0,0));
