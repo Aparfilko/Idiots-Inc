cee16ff5855c1b54e55f41112a86503b8754854a
Doots <65699806+Dootsu@users.noreply.github.com>
2020-07-10 21:58:24 -0400

    Semi-functional
    it do jump

--- Brakes Optional/assets/scene/car.gd
+++ Brakes Optional/assets/scene/car.gd
@@ -6,7 +6,12 @@ onready var ass_mat=[
 func _ready():
 	pass # Replace with function body.
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _input(event):
+	if(event.is_action_pressed("w")):
+		translate(Vector3(0,0,1));
+	if(event.is_action_pressed("s")):
+		translate(Vector3(0,0,-1));
+	if(event.is_action_pressed("a")):
+		rotate(Vector3(0,1,0),.1);
+	if(event.is_action_pressed("d")):
+		rotate(Vector3(0,1,0),-.1);
