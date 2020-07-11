fb431f0f713370e6130619ecb95f2b6257d6e07b
Doots <65699806+Dootsu@users.noreply.github.com>
2020-07-10 21:13:27 -0400

    car
    look at the car

---
+++ Brakes Optional/assets/scene/car.gd
@@ -0,0 +1,12 @@
+extends KinematicBody
+
+onready var ass_mat=[
+];
+
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
--- Brakes Optional/script/car.gd
+++ Brakes Optional/script/car.gd
@@ -1,5 +1,7 @@
 extends KinematicBody
 
+
+
 func _ready():
 	pass # Replace with function body.
 
