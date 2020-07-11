726020a8774ed3377cca8a3eb5e6821280b4e5c5
Aparfilko <are.they.giants@gmail.com>
2020-07-11 00:45:04 -0400

    Alex you try

--- Brakes Optional/script/booster.gd
+++ Brakes Optional/script/booster.gd
@@ -1,12 +1,13 @@
 extends MeshInstance
 
 var ass_mat;
-	
+
 func set_ass(a):
 	$ass.material=ass_mat[a];
 func _process(delta):
+	#print($ass.mesh.surface_get_material(0).resource_name)
 	$RedPuff.translate(Vector3(0,1,0))
-	$RedPuff.translate(Vector3(0,0.9,0))
+	$RedPuff.translate(Vector3(0,2,0))
 	if $RedPuff.translation.z >= 50:
 		$RedPuff.translation.z = -0.5
 	if $BlackPuff.translation.z >= 50:
