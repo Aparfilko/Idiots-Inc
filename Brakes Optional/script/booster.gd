0b993e2b4c3fc62b82098fd3d8070af08cf980ae
Aparfilko <are.they.giants@gmail.com>
2020-07-11 00:08:51 -0400

    messing with smoke

--- Brakes Optional/script/booster.gd
+++ Brakes Optional/script/booster.gd
@@ -5,11 +5,16 @@ var ass_mat;
 func set_ass(a):
 	$ass.material=ass_mat[a];
 func _process(delta):
-	print($ass.mesh.surface_get_material(0))
-	$RedPuff.translate(Vector3(0,2,0))
+	$RedPuff.translate(Vector3(0,1,0))
+	$RedPuff.translate(Vector3(0,0.9,0))
 	if $RedPuff.translation.z >= 50:
 		$RedPuff.translation.z = -0.5
-	#print($ass.mesh.material.shader.resource_name)# == "on":
-	#	$RedPuff.visible = true
-	#else:
-	#	$RedPuff.visible = false
+	if $BlackPuff.translation.z >= 50:
+		$BlackPuff.translation.z = -0.5
+		
+	if $ass.mesh.resource_name == "ON":
+		$RedPuff.visible = true
+		$BlackPuff.visible = true
+	else:
+		$RedPuff.visible = false
+		$BlackPuff.visible = false
