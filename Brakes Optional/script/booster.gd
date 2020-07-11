62a3aafb6ac3a687b2dc1f40d775ba0498a17edc
Doots <65699806+Dootsu@users.noreply.github.com>
2020-07-11 00:53:51 -0400

    Ball Control
    they were unruly

--- Brakes Optional/script/booster.gd
+++ Brakes Optional/script/booster.gd
@@ -5,7 +5,7 @@ var ass_mat;
 func set_ass(a):
 	$ass.material=ass_mat[a];
 func _process(delta):
-	#print($ass.mesh.surface_get_material(0).resource_name)
+	print($ass.get_surface_material(0).resource_name)
 	$RedPuff.translate(Vector3(0,1,0))
 	$RedPuff.translate(Vector3(0,2,0))
 	if $RedPuff.translation.z >= 50:
@@ -13,7 +13,7 @@ func _process(delta):
 	if $BlackPuff.translation.z >= 50:
 		$BlackPuff.translation.z = -0.5
 		
-	if $ass.mesh.resource_name == "ON":
+	if $ass.get_surface_material(0).resource_name == "ON":
 		$RedPuff.visible = true
 		$BlackPuff.visible = true
 	else:
