3893d7325254179bcfafe754de32c7e44ea0b3ba
Aparfilko <are.they.giants@gmail.com>
2020-07-10 23:25:24 -0400

    messing around with ass

--- Brakes Optional/assets/scene/booster.gd
+++ Brakes Optional/assets/scene/booster.gd
@@ -5,10 +5,11 @@ var ass_mat;
 func set_ass(a):
 	$ass.material=ass_mat[a];
 func _process(delta):
+	print($ass.mesh.surface_get_material(0))
 	$RedPuff.translate(Vector3(0,2,0))
 	if $RedPuff.translation.z >= 50:
 		$RedPuff.translation.z = -0.5
-	print($ass.mesh.material.shader.resource_name)# == "on":
+	#print($ass.mesh.material.shader.resource_name)# == "on":
 	#	$RedPuff.visible = true
 	#else:
 	#	$RedPuff.visible = false
