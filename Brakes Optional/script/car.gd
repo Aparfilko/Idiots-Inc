dd7e605c2dd0f675335be70f9831e6e35b1c57b3
Doots <65699806+Dootsu@users.noreply.github.com>
2020-07-11 05:10:59 -0400

    E to float
    bouncy

--- Brakes Optional/script/car.gd
+++ Brakes Optional/script/car.gd
@@ -8,16 +8,23 @@ var angVel=0;
 var b=[0,0,0,0,0,0];#ign,thrust,lev,left,brake,right
 
 func _ready():
+	$RayCast.cast_to=Vector3(0,-20,0);
 	pass # Replace with function body.
 
 func _physics_process(delta):
+	var q=Quat(Vector3(0,1,0),rotation[1]);
 	angVel+=(.1*delta if b[3] else 0)-(.1*delta if b[5] else 0);
-	vel+=Quat(Vector3(0,1,0),rotation[1])*Vector3(0,0,(10*delta if b[1] else 0)+(3*delta if b[3] else 0)+(3*delta if b[5] else 0)-(10*delta if b[4] else 0));
-#	vel+=Vector3(1,0,0)*vel.dot(Quat(Vector3(0,1,0),rotation[1])*Vector3(1,0,0))
+	vel+=q*Vector3(0,0,(10*delta if b[1] else 0)+(3*delta if b[3] else 0)+(3*delta if b[5] else 0)-(10*delta if b[4] else 0));
+	vel-=q*Vector3(.5,0,0)*vel.dot(q*Vector3(1,0,0))
+	if($RayCast.is_colliding()):
+		var d=(translation[1]-$RayCast.get_collision_point()[1]);
+		if(d<scale[1]*(20 if b[2] else 10)):
+			vel[1]+=(scale[1]*(20 if b[2] else 10)-d)*20*delta;
+	vel[1]-=9*delta;
+	vel[1]*=.99;
 	rotation[1]+=angVel;
-	var col=move_and_slide(vel);
-	if(col):
-		pass;
+	var col_=move_and_slide(vel);
+	
 
 func _input(event):
 	b=[
