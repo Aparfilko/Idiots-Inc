e89531d6401667f39d42ba71b24aff55fdf53e12
whatislostinthemines <whatislostinthemines@gmail.com>
2020-07-12 00:34:40 -0400

    win screen + pause screen evolved

--- Brakes Optional/script/pausemenu.gd
+++ Brakes Optional/script/pausemenu.gd
@@ -4,11 +4,13 @@ extends Control
 # Declare member variables here. Examples:
 # var a = 2
 var pauseState = false
+var halt
 onready var screenInit = Vector2(1920, 1080)
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	halt = false
 	visible = pauseState
 	get_tree().paused = pauseState
 	resize()
@@ -17,10 +19,12 @@ func resize():
 	var x = OS.get_window_size().x/screenInit.x
 	var y = OS.get_window_size().y/screenInit.y
 	set_scale(Vector2(x, y))
+	
 func _input(event):
-	if event.is_action_pressed("ui_cancel"):
-		pauseState = true
-		visible = pauseState
+	if event.is_action_pressed("ui_cancel") and not halt:
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
+		pauseState = not pauseState
+		visible = not visible
 		get_tree().paused = pauseState
 
 func _on_Resume_pressed(): #resume
@@ -29,12 +33,15 @@ func _on_Resume_pressed(): #resume
 	get_tree().paused = pauseState
 
 func _on_Restart_pressed(): #restart track
-	pass # Replace with function body.
+	pauseState = false
+	visible = pauseState
+	get_tree().paused = pauseState
+	var levelMan = get_parent().get_parent().get_node("ManagerLevel")
+	levelMan.emit_signal("reset")
+	levelMan.refCar.transform=levelMan.refCurr.get_node("SpawnPoint").transform;
 
 func _on_BackToStart_pressed(): #Return to either start screen or whatever main 
-	#we develop
-	#visible = not visible
-	pass # Replace with function body.
+	get_tree().change_scene("res://overlay/TitleScreen.tscn")
 
 func _on_Quit_pressed(): #quit game. no dialogue to confirm that yet.
 	get_tree().quit();
