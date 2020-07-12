67fc92f1d007b6af323164cae39e0bc68982804f
whatislostinthemines <whatislostinthemines@gmail.com>
2020-07-11 22:23:23 -0400

    alter manager level to actually work, and death also title

--- Brakes Optional/script/ManagerLevel.gd
+++ Brakes Optional/script/ManagerLevel.gd
@@ -3,16 +3,26 @@ extends Node
 var refOverlay;
 var refCurr;
 var refCar;
-onready var resLevel=[
-	preload("res://level/Track1.tscn"),
-];
+onready var cnt = 1
+onready var resLevel = preload("res://level/Track1.tscn")
 
 func _ready():
 	refCar=get_parent().get_node("car");
-	refCurr=resLevel[0].instance();
+	nextOne()
+
+func nextOne():
+	refCurr=resLevel.instance();
+	print(refCurr.name)
 	add_child(refCurr);
 	refCar.transform=refCurr.get_node("SpawnPoint").transform;
 
-
 func _on_DeathZone_body_entered(body):
+	print("welp")
 	pass # Replace with function body.
+	
+func next_level():
+	cnt += 1
+	remove_child(refCurr)
+	refCurr.queue_free()
+	resLevel = load("res://level/Track"+String(cnt)+".tscn")
+	nextOne()
