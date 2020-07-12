961e0ba4f87e96e4a83853fd2649821c1c08700a
whatislostinthemines <whatislostinthemines@gmail.com>
2020-07-11 21:27:07 -0400

    some changes to checkpoints

--- Brakes Optional/script/Checkpoints.gd
+++ Brakes Optional/script/Checkpoints.gd
@@ -1,31 +1,22 @@
 extends Spatial
-
-onready var Area1 = false
-onready var Area2 = false
-onready var Area3 = true
+enum {NEXT1, NEXT2, NEXT3}
+var goals = NEXT2
 var lap = 0
 
+
 func _on_Area1_body_entered(body):
-	if Area1:
+	if goals == NEXT1:
 		print(lap)
-		Area1 = false
-		Area2 = true
-		Area3 = false
+		goals = NEXT2
 		lap+=1
-		print(lap)
 		if lap == 3:
-			get_node("/root/MainNode/car/Hud/WinState").popup()
+			get_node("/root/MainNode/car/Hud/WinState").popup_centered()
 
 func _on_Area2_body_entered(body):
-	if Area2:
-		print(lap)
-		Area1 = false
-		Area2 = false
-		Area3 = true
+	if goals == NEXT2:
+		goals = NEXT3
 
 func _on_Area3_body_entered(body):
-	if Area3:
+	if goals == NEXT3:
 		print(lap)
-		Area1 = true
-		Area2 = false
-		Area3 = false
+		goals = NEXT1
