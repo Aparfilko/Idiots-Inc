37692cb9f55a4ddf065e7b74cba262cf574c0e4b
Aparfilko <are.they.giants@gmail.com>
2020-07-12 00:17:26 -0400

    fixing demo

--- Brakes Optional/Demo.gd
+++ Brakes Optional/Demo.gd
@@ -10,7 +10,7 @@ var speech = ["Welcome to the RADICAL HYPER MEGA TRACK!",
 	"Check her out...",
 	"Hop inside and I'll show you how to start the engine pods!"]
 var n = 0
-
+var mat = load("res://assets/scene/off.material")
 
 onready var size = get_viewport().size
 
@@ -18,9 +18,9 @@ onready var size = get_viewport().size
 func _ready():
 	$Dialog/Label.text = speech[n]
 	$Camera.current = true
-	$car/body/booster/ass.set_surface_material(0,load("res://assets/scene/off.material"))
-	$car/body/booster2/ass.set_surface_material(0,load("res://assets/scene/off.material"))
-	$car/body/booster3/ass.set_surface_material(0,load("res://assets/scene/off.material"))
+	$car/body/booster/ass.set_surface_material(0,mat)
+	$car/body/booster2/ass.set_surface_material(0,mat)
+	$car/body/booster3/ass.set_surface_material(0,mat)
 	$Dialog.visible = true
 	$Dialog.rect_position.x = size[0]/2-300
 	$Dialog.rect_size = Vector2(600,250)
@@ -31,9 +31,12 @@ func _input(event):
 	if event is InputEventMouseMotion:
 		pass
 	else:
-		if n < speech.size()-1 and $Dialog/Label.percent_visible == 1:
-			n += 1
-			$Dialog/Label.text = speech[n]
-			$Dialog/Label.percent_visible = 0
+		if n < speech.size()-1:
+			if $Dialog/Label.percent_visible == 1:
+				n += 1
+				$Dialog/Label.text = speech[n]
+				$Dialog/Label.percent_visible = 0
+			else:
+				$Dialog/Label.percent_visible = 1
 		if n == speech.size()-1 and $Dialog/Label.percent_visible == 1:
 			load("MainNode.tscn")
