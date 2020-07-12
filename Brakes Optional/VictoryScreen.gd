14acf1450be50b74ea80c2077ebcfc710926ed06
Minerette <anjali.jothi@gmail.com>
2020-07-12 14:11:41 -0400

    St. Dave and cake added to victory screen

--- Brakes Optional/VictoryScreen.gd
+++ Brakes Optional/VictoryScreen.gd
@@ -3,5 +3,8 @@ extends Node2D
 
 func _input(event):
 	if event.is_action_pressed("ui_cancel"):
-		$global.level=0;
 		get_tree().change_scene("res://MainNode.tscn")
+
+
+func _on_Button_pressed():
+	get_tree().change_scene("res://MainNode.tscn")
