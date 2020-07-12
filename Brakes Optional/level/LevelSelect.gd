cd66df6711ca6440e6b21899714f4f46ccb36f5b
doublebman13 <doublebman13@gmail.com>
2020-07-12 01:10:32 -0400

    Trying to level select

--- Brakes Optional/level/LevelSelect.gd
+++ Brakes Optional/level/LevelSelect.gd
@@ -8,10 +8,10 @@ func _on_Tutorial_pressed():
 	pass
 
 func _on_Track1_pressed():
-	get_tree().change_scene("res://level/Track1.tscn")
+	get_tree().get_child("ManagerLevel").choose_level(1)
 
 func _on_Track2_pressed():
-	get_tree().change_scene("res://level/Track2.tscn")
+	get_tree().get_child("ManagerLevel").choose_level(2)
 
 func _on_Track3_pressed():
-	get_tree().change_scene("res://level/Track3.tscn")
+	get_tree().get_child("ManagerLevel").choose_level(3)
