1ac4b4ebc95dfbdd9d03ffc7e744f7d560a9e481
whatislostinthemines <whatislostinthemines@gmail.com>
2020-07-12 07:45:28 -0400

    improved title screen

--- Brakes Optional/script/StartScreemb.gd
+++ Brakes Optional/script/StartScreemb.gd
@@ -4,11 +4,7 @@ extends Panel
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+onready var node = preload("res://Demo.tscn").instance()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -17,7 +13,8 @@ func _ready():
 
 #startGame
 func _on_start_pressed():
-	get_tree().change_scene("res://Demo.tscn")
+	visible = false
+	get_parent().add_child(node)
 
 
 #exit, probably should have a dialog box?
