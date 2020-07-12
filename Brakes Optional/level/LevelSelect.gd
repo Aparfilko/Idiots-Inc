64ec606a5784a3b6f4d59d5df0f86bd93a489c2b
doublebman13 <doublebman13@gmail.com>
2020-07-11 21:49:36 -0400

    LevelSelect

---
+++ Brakes Optional/level/LevelSelect.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+
+func _on_Back_pressed():
+	pass
+
+func _on_Tutorial_pressed():
+	pass
+
+func _on_Track1_pressed():
+	pass
+
+func _on_Track2_pressed():
+	pass
+
+func _on_Track3_pressed():
+	pass
