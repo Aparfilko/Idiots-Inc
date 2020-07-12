648e00ccb437038db0b21618a3634fd0d35b93c8
Minerette <anjali.jothi@gmail.com>
2020-07-11 20:35:10 -0400

    start screen kinda works

---
+++ Brakes Optional/script/StartScreemb.gd
@@ -0,0 +1,30 @@
+extends Panel
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+#startGame
+func _on_start_pressed():
+	get_tree().change_scene("res://MainNode.tscn")
+
+
+#view tutorial? idk
+func _on_controls_pressed():
+	pass # get_tree().change_scene(<CONTROLS SCENE HERE>)
+
+
+#exit, probably should have a dialog box?
+func _on_LeabDisPwace_pressed(): #uwu
+	get_tree().quit();
