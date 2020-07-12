e224b55bcf7854e88eb23ef3efdd36ae8ebe6d8b
whatislostinthemines <whatislostinthemines@gmail.com>
2020-07-12 14:22:11 -0400

    title fixed

--- Brakes Optional/VictoryScreen.gd
+++ Brakes Optional/VictoryScreen.gd
@@ -1,5 +1,12 @@
 extends Node2D
 
+func _ready():
+	resize(Vector2(1920,1080))
+func resize(screenInit):
+	var x = OS.get_window_size().x/screenInit.x
+	var y = OS.get_window_size().y/screenInit.y
+	set_scale(Vector2(x, y))
+
 
 func _input(event):
 	if event.is_action_pressed("ui_cancel"):
@@ -8,4 +15,9 @@ func _input(event):
 
 
 func _on_Button_pressed():
+	$buttonSfx/click.play()
 	get_tree().change_scene("res://overlay/TitleScreen.tscn")
+
+
+func _on_button_hover():
+	$buttonSfx/hover.play()
