6ff274cf6aed3879e61ad6f540c6ca2523229a7a
Doots <65699806+Dootsu@users.noreply.github.com>
2020-09-01 22:28:35 -0400

    wejump
    realhigh

--- TerrainDemo/Player.gd
+++ TerrainDemo/Player.gd
@@ -4,6 +4,7 @@ onready var pTerrainGen=get_parent().get_node("TerrainGen");
 var velPast=Vector3();
 var velTarget=Vector3();
 var velCurr=Vector3();
+var velY=0;
 var timePast=0;
 var timeCurr=0;
 
@@ -20,10 +21,20 @@ func _input(event):
 		timePast=timeCurr;
 		velPast=velCurr;
 		velTarget=velNew;
+	if(event.is_action_pressed("space")):
+		velY=50;
 
 func _process(dt):
-	timeCurr+=5*dt;
-	var r=exp(timePast-timeCurr);
+	timeCurr+=dt;
+	var r=exp(10*(timePast-timeCurr));
 	velCurr=(r*velPast+(1-r)*velTarget);
+	
+	velY-=98.1*dt;
+	if(velY<0 and translation[1]<pTerrainGen._worldHeight(translation[0],translation[2])):
+		velY=0;
+		translation[1]=pTerrainGen._worldHeight(translation[0],translation[2]);
+	
 	translation+=velCurr*10*dt;
-	translation[1]=pTerrainGen._worldHeight(translation[0],translation[2]);
+	translation[1]+=velY*dt;
+	
+	$MeshInstance.rotation=Vector3(.2*velCurr[2],0,-.2*velCurr[0]);
